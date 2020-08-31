38a6b9be109a5976a40133491834797c3b60ce81
Viktorlazi <viktorlazic11@gmail.com>
2020-08-31 14:30:16 +0200

    ulipsa

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Login from './components/Login/Login';
 import Register from './components/Login/Register';
+import Notes from './components/App/Notes'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Login/>
       </Route>
       <Route exact path="/">
-        <h1>main</h1>
+        <Notes/>
       </Route>
       <Route exact path="/register">
         <Register/>
---
+++ src/components/App/Notes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './notes.css'
+
+export default class Notes extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>ej</h1>
+            </div>
+        )
+    }
+}
