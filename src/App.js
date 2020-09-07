b6a4fce3c1d77713be47c66976b761ab410c0c60
Viktorlazi <viktorlazic11@gmail.com>
2020-09-07 21:21:44 +0200

    uredia za internet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, BrowserRouter} from 'react-router-dom'
 import Login from './components/Login/Login';
 import Register from './components/Login/Register';
 import Notes from './components/App/Notes'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/notes">
       <Route exact path="/login">
         <Login/>
       </Route>
@@ -17,7 +17,7 @@ function App() {
       <Route exact path="/register">
         <Register/>
       </Route>
-    </Router>
+    </BrowserRouter>
   );
 }
 
